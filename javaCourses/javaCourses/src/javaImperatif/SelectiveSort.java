140346898d3e5864b20e525f8da65a5b57a28a39
Boris BELLOC <bb.itwork@gmail.com>
2019-10-04 17:23:43 +0200

    tp10 updated

--- javaCourses/javaCourses/src/javaImperatif/SelectiveSort.java
+++ javaCourses/javaCourses/src/javaImperatif/SelectiveSort.java
@@ -10,11 +10,20 @@ public class SelectiveSort {
 
 		for (int i = 0; i < tab.length; i++) {
 
-			tabTemp[i] = (int) null;
-			System.out.println("Tri en cours -> " + Arrays.toString(tabTemp));
+			// tabTemp[i] = null;
+			// System.out.println("Tri en cours -> " + Arrays.toString(tabTemp));
 
 		}
 
 	}
 
+	// procedure tri_selection(tableau t, entier n)
+	// pour i de 0 à n - 2
+//	    min ← i       
+//	    pour j de i + 1 à n - 1
+//	        si t[j] < t[min], alors min ← j
+//	    fin pour
+//	    si min ≠ i, alors echanger t[i] et t[min]
+	// fin pour
+	// fin procédure
 }
