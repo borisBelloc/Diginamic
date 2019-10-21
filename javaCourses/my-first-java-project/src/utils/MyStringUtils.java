764c8b5ba6304c86ab64d8a262c73c4e8e2ea95b
Boris BELLOC <bb.ItWork@gmail.com>
2019-10-21 21:36:20 +0200

    updated

--- javaCourses/my-first-java-project/src/utils/MyStringUtils.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class MyStringUtils {
-
-	public static boolean isNullOrEmpty(String stringToTest) {
-		return stringToTest == null || stringToTest.trim().isEmpty();
-	}
-
-}
