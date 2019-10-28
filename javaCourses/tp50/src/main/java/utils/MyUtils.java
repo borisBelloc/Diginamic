9d183ff455bf3e4956baba1ba406636d300608a0
Boris BELLOC <bb.itwork@gmail.com>
2019-10-28 18:06:51 +0100

    tp 50 ok en stream

--- javaCourses/tp50/src/main/java/utils/MyUtils.java
+++ javaCourses/tp50/src/main/java/utils/MyUtils.java
@@ -2,6 +2,8 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class MyUtils {
 
@@ -13,14 +15,30 @@ public class MyUtils {
 	 * @author BELLOC Boris
 	 */
 	public int sum(List<Integer> list) {
-		int total = 0;
-		for (Integer i : list) {
-			i = i == null ? 0 : i;
-			total += i;
-		}
+//		int total = 0;
+//		for (Integer i : list) {
+//			i = i == null ? 0 : i;
+//			total += i;
+//		}
+// CLASSIC		
+		
+		// doc stream : https://www.baeldung.com/java-stream-sum
+		
+		
+		
+		// Version en stream
+		Integer total = list.stream().filter(i -> i != null)
+				.mapToInt(Integer::intValue).sum();
 		return total;
 	}
 
+	/*
+	 * Integer sum = integers.stream()
+  		.reduce(0, Integer::sum);
+	 * 
+	 */
+	
+	
 	/**
 	 * Retourne null si s est vide, sinon retourne s
 	 * 
@@ -33,7 +51,6 @@ public class MyUtils {
 		} else {
 			return s;
 		}
-
 	}
 
 	/**
@@ -42,19 +59,34 @@ public class MyUtils {
 	 * @param integers
 	 * @return
 	 */
+//	public List<Integer> filterEvenNumbers(List<Integer> integers) {
+//
+//		List<Integer> filtered = new ArrayList<>();
+//
+//		for (Integer i : integers) {
+//			if (i != null) {
+//				if (i % 2 == 0) {
+//					filtered.add(i);
+//				}
+//			}
+//		}
+//		return filtered;
+//	} // version without stream
+	
+
+	
 	public List<Integer> filterEvenNumbers(List<Integer> integers) {
 
 		List<Integer> filtered = new ArrayList<>();
 
-		for (Integer i : integers) {
-			if (i != null) {
-				if (i % 2 == 0) {
-					filtered.add(i);
-				}
-			}
-		}
+		filtered = integers.stream()
+				.filter(i -> i != null && i % 2 == 0)
+				.collect(Collectors.toList());
+		
 		return filtered;
 	}
+	
+	
 	
 	
 	/**
@@ -62,18 +94,33 @@ public class MyUtils {
 	 * @param integers
 	 * @return
 	 */
+//	public List<Float> transformSquarePlusPointFive(List<Integer> integers) {
+//		List<Float> transformed = new ArrayList<>();
+//		float temp;
+//		
+//		for (Integer i : integers) {
+//			temp = ((i * i) + 0.5f);
+//			transformed.add(temp);
+//		}
+//		
+////		for (float i : transformed) {
+////			System.out.println("i -> " + i);
+////		}
+//		
+//		return transformed;
+//		
+//	}
+	
+	
+	
 	public List<Float> transformSquarePlusPointFive(List<Integer> integers) {
 		List<Float> transformed = new ArrayList<>();
-		float temp;
 		
-		for (Integer i : integers) {
-			temp = ((i * i) + 0.5f);
-			transformed.add(temp);
-		}
-		
-//		for (float i : transformed) {
-//			System.out.println("i -> " + i);
-//		}
+		// TODO : traitement en cas de param null
+		transformed = integers.stream()
+				.filter(i -> i != null)
+				.map(i -> (float)Math.pow(i,2)+0.5f)
+				.collect(Collectors.toList());
 		
 		return transformed;
 		
