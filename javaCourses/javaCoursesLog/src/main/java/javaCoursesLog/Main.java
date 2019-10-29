d9d476e1bd6dfb80283e61f192692e4ab21bddeb
Boris BELLOC <bb.itwork@gmail.com>
2019-10-29 15:34:17 +0100

    Java course log

---
+++ javaCourses/javaCoursesLog/src/main/java/javaCoursesLog/Main.java
@@ -0,0 +1,18 @@
+package javaCoursesLog;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	
+	private static Logger logger = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		
+
+//		System.out.println("aa");
+		logger.debug("hello");
+
+	}
+
+}
