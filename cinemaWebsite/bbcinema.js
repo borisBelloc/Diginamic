9fa70ad388b66da020c1da6e316b22b0830c0d2c
Boris BELLOC <bb.itwork@gmail.com>
2019-10-30 18:13:07 +0100

    fin journée

--- cinemaWebsite/bbcinema.js
+++ cinemaWebsite/bbcinema.js
@@ -1,13 +1,13 @@
 // Navbar shadow on scroll
 $(window).scroll(function () {
-  if ($(window).scrollTop() > 100) {
+  if ($(window).scrollTop() > 500) {
     $('.navbar').addClass('floatingNav');
     $('.navbar').removeClass('transparent');
     $('.nav-link').removeClass('displayNone');
   } else {
     // $('.navbar').removeClass('floatingNav');
   }
-  if ($(window).scrollTop() < 50) {
+  if ($(window).scrollTop() < 500) {
     $('.navbar').addClass('transparent');
     $('.nav-link').addClass('displayNone');
     $('.navbar').removeClass('floatingNav');
