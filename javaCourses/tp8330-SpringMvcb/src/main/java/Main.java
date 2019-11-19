aa290aba5534a9d4c6abdd6908e40abc7de20062
Boris BELLOC <bb.itwork@gmail.com>
2019-11-19 10:00:24 +0100

    restart tp8330 Sprinv MVC

--- javaCourses/tp8330-SpringMvcb/src/main/java/Main.java
+++ javaCourses/tp8330-SpringMvcb/src/main/java/Main.java
@@ -1,5 +1,3 @@
-package fr.diginamic.tp8330_SpringMvc;
-
 public class Main {
 
 	public static void main(String[] args) {
---
+++ javaCourses/tp8330-SpringMvcb/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Dans le fichier index.jsp</h2>
+</body>
+</html>
