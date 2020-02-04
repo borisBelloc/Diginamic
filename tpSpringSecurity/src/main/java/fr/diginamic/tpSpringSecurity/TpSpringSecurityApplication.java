0791d521e4ec31f0c3312cdc47fed065269effb8
Boris BELLOC <bb.itwork@gmail.com>
2020-02-04 10:10:12 +0100

    spring security from Spring boot ini

---
+++ tpSpringSecurity/src/main/java/fr/diginamic/tpSpringSecurity/TpSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.tpSpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringSecurityApplication.class, args);
+	}
+
+}
