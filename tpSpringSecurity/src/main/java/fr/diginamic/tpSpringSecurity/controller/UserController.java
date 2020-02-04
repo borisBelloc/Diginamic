c4a9e4db8953b01a722360efeb3f3450ed169f5c
Boris BELLOC <bb.itwork@gmail.com>
2020-02-04 16:57:54 +0100

    ok before security conf

--- tpSpringSecurity/src/main/java/fr/diginamic/tpSpringSecurity/controller/UserController.java
+++ tpSpringSecurity/src/main/java/fr/diginamic/tpSpringSecurity/controller/UserController.java
@@ -3,25 +3,48 @@ package fr.diginamic.tpSpringSecurity.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.diginamic.tpSpringSecurity.exception.AlreadyExistsException;
+import fr.diginamic.tpSpringSecurity.exception.NotFoundException;
 import fr.diginamic.tpSpringSecurity.models.User;
 import fr.diginamic.tpSpringSecurity.service.UserService;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class UserController {
 	
 	@Autowired
-	private UserService service;
+	private UserService userService;
 	
+	// @GetMapping <- peut remplacer le requestMethod.GET
 	@RequestMapping(method = RequestMethod.GET)
 	public List<User> findAll() {
-		return service.findAll();
+		return userService.findAll();
 	}
 	
+	@GetMapping("/{id}")
+	public User findById(@PathVariable Long id) throws NotFoundException {
+		return userService.findById(id);
+	}
+	
+	@PostMapping
+	public User create(@RequestBody User user) throws AlreadyExistsException {
+		return userService.create(user);
+	}
+	
+	@DeleteMapping("/{id}")
+	public User delete(@PathVariable Long id) throws NotFoundException {
+		return userService.delete(id);
+	}
+
 	
 
 }
--- tpSpringSecurity/src/main/java/fr/diginamic/tpSpringSecurity/exception/NotFoundException.java
+++ tpSpringSecurity/src/main/java/fr/diginamic/tpSpringSecurity/exception/NotFoundException.java
@@ -1,5 +1,11 @@
 package fr.diginamic.tpSpringSecurity.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+// Si exeption levé par controller -> reponse http 404 "not found"
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Object not found")
 public class NotFoundException extends Exception {
 
 	private static final long serialVersionUID = -8089144995057256430L;
