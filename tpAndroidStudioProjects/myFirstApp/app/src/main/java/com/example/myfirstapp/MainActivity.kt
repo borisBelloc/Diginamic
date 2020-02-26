c45912467d31e0817b8aa3b1fcc1645e949ef7e7
Boris BELLOC <bb.itwork@gmail.com>
2020-02-26 17:01:13 +0100

    tp3

--- tpAndroidStudioProjects/myFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ tpAndroidStudioProjects/myFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.tp3photogallery.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tp2relativelayout)
+        setContentView(R.layout.tp3photogallery)
+
+//        textViewDescriptionCity.text = "exemple"
+
+        buttonDiscard.setOnClickListener {
+//            textViewDescriptionCity.text = "CANCELED"
+            Toast.makeText(applicationContext, "ETES VOUS SURE ???", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
