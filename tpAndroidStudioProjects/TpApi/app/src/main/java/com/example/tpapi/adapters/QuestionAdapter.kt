f90bb08e74246f50200bc0ac84e901352151e4cc
Boris BELLOC <bb.itwork@gmail.com>
2020-02-28 14:34:18 +0100

    added link to post

--- tpAndroidStudioProjects/TpApi/app/src/main/java/com/example/tpapi/adapters/QuestionAdapter.kt
+++ tpAndroidStudioProjects/TpApi/app/src/main/java/com/example/tpapi/adapters/QuestionAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.tpapi.adapters
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
@@ -7,7 +9,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.tpapi.R
 import com.example.tpapi.api.models.Item
 import com.example.tpapi.api.models.Questions
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_question.view.*
+import retrofit2.http.Url
 
 class QuestionAdapter (val questions: List<Item>) : RecyclerView.Adapter<QuestionViewHolder>() {
 
@@ -35,12 +39,22 @@ class QuestionViewHolder (inflater: LayoutInflater, parent: ViewGroup)
     fun bind(question: Item) {
 
 
+
         itemView.apply {
             textDisplayName.text = question.title
 //            textDisplayName.text = question.owner.displayName
 //            textCreationDate.text = question.items.creationDate.toString()
+
+            textClick.setOnClickListener{
+                val myIntent = Intent(Intent.ACTION_VIEW)
+                myIntent.data = Uri.parse(question.link)
+                itemView.context.startActivity(myIntent)
+            }
+
+            Picasso.get().load(question.owner.profileImage).into(imgProfileImage);
+
 //            imgProfileImage.setImageDrawable(
-//                ContextCompat.getDrawable(itemView.context, contact.avatar))
+//                ContextCompat.getDrawable(itemView.context, question.owner.profileImage))
         }
     }
 }
